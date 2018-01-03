acd070cfbbbde7fae606576c50735a52bb4449c3
Daniel Brown <drbrown01@yahoo.com>
2018-01-03 16:38:28 -0600

    Added liri.js, keys.js, readme-update, log.txt and random.txt

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("Keys Loaded");
+
+//.Get API credentials
+
+var twitterKeys ={
+  consumer_key: "SZzsigw3iqE0pSADBUJkENCab",
+  consumer_secret: "AMw4tYFP7an0lIul49C21aS9fk2cWZTGS8XVE9bjb2JFOlNa36",
+  access_token_key: "2722037155-UARaU6pdJvvjerc1jKNuZXxprSnk4bPlbIHqY81",
+  access_token_secret: "wnpv3qmcRgT4tBLvzFEnPAqU9ih2fKkLtbE95ZIFXAaQo"
+
+};
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,3 @@
+//dependencies
+//import the keys files
+var keys = require("./keys.js")
